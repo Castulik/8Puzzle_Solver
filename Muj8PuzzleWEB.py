160a4e2af6d1f73f11fe927b8fd1d5fca15f0cb5
Castulik <filip.castulik@seznam.cz>
2025-12-30 22:15:33 +0100

    snad

--- Muj8PuzzleWEB.py
+++ Muj8PuzzleWEB.py
@@ -1,8 +1,167 @@
-import queue1
-import stack1
 import heapq
 import asyncio  # NUTNÉ PRO WEB
 
+# Queue - FIFO - Fronta
+class Node:
+    def __init__(self, data):
+        self.__next = None
+        self.__prev = None
+        self.__data = data
+
+    def get_data(self):
+        return self.__data
+
+    def set_next(self, nextt):
+        self.__next = nextt
+
+    def set_prev(self, prev):
+        self.__prev = prev
+
+    def get_next(self):
+        return self.__next
+
+    def get_prev(self):
+        return self.__prev
+
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def is_empty(self):
+        if self.head == None:
+            return True
+        else:
+            return False
+
+    def get_size(self):
+        return self.size
+
+    def push(self, data):
+        temp = Node(data)
+
+        if self.is_empty():
+            self.head = temp
+            self.tail = temp
+        else:
+            self.tail.set_next(temp)
+            temp.set_prev(self.tail)
+            self.tail = temp
+
+        self.size += 1
+
+    def pop(self):
+        if self.is_empty():
+            raise Exception("Queue is empty!")
+
+        else:
+            data = self.head.get_data()
+
+            if self.size > 1:
+                next_node = self.head.get_next()
+                next_node.set_prev(None)
+                self.head = next_node
+            else:
+                self.head = None
+                self.tail = None
+
+            self.size -= 1
+
+            return data
+
+    def print_list(self):
+        current = self.head
+
+        while current != None:
+            print(current.get_data())
+            current = current.get_next()
+            
+# Stack - LIFO - Zasobnik
+
+
+class Node:
+    def __init__(self, data):
+        self.__next = None
+        self.__prev = None
+        self.__data = data
+
+    def get_data(self):
+        return self.__data
+
+    def set_next(self, nextt):
+        self.__next = nextt
+
+    def set_prev(self, prev):
+        self.__prev = prev
+
+    def get_next(self):
+        return self.__next
+
+    def get_prev(self):
+        return self.__prev
+
+
+class Stack:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def is_empty(self):
+        if self.head == None:
+            return True
+        else:
+            return False
+
+    def get_size(self):
+        return self.size
+
+    def push(self, data):
+        temp = Node(data)
+
+        if self.is_empty():
+            self.head = temp
+            self.tail = temp
+        else:
+            self.tail.set_next(temp)
+            temp.set_prev(self.tail)
+            self.tail = temp
+
+        self.size += 1
+
+    def pop(self):
+
+        if self.is_empty():
+            raise Exception("Stack is empty!")
+        else:
+            data = self.tail.get_data()
+
+            if self.size > 1:
+                prev = self.tail.get_prev()
+                prev.set_next(None)
+                self.tail = prev
+            else:
+                self.tail = None
+                self.head = None
+
+            self.size -= 1
+            return data
+
+    def peek(self):
+        if not self.is_empty():
+            return self.tail.get_data()
+        else:
+            raise Exception("Stack is empty!")
+
+    def print_list(self):
+        current = self.head
+
+        while current != None:
+            print(current.get_data())
+            current = current.get_next()
+            
 TARGET_POSITIONS = {
     1: (0, 0), 2: (0, 1), 3: (0, 2),
     4: (1, 0), 5: (1, 1), 6: (1, 2),
@@ -50,7 +209,7 @@ class PuzzleSolver:
 
     async def solve_puzzle_bfs(self):
         self.prozkoumano = 0 
-        fifo = queue1.Queue()
+        fifo = Queue()
         fifo.push(self.root)
         visited = {tuple(map(tuple, self.root.data))}
         
@@ -80,7 +239,7 @@ class PuzzleSolver:
 
     async def solve_puzzle_dfs(self):
         self.prozkoumano = 0
-        stack = stack1.Stack()
+        stack = Stack()
         stack.push(self.root)
         visited = {tuple(map(tuple, self.root.data))}
         
@@ -108,7 +267,7 @@ class PuzzleSolver:
 
     async def solve_puzzle_dfs_limit(self):
         self.prozkoumano = 0
-        stack = stack1.Stack()
+        stack = Stack()
         stack.push(self.root)
         visited = {tuple(map(tuple, self.root.data)): 0}
         
